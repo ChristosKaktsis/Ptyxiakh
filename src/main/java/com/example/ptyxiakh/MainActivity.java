3874a40891944d43e83f1b237ff28bdd03019b02
ChristosKaktsis <christoskaktsis@gmail.com>
2021-03-10 21:55:24 +0200

    First Step XML only

--- src/main/java/com/example/ptyxiakh/MainActivity.java
+++ src/main/java/com/example/ptyxiakh/MainActivity.java
@@ -3,18 +3,33 @@ package com.example.ptyxiakh;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     FrameLayout flfragment ; //Layout that fragments will appear
     Fragment main_fragment = new Main_Fragment();
     Fragment profile_fragment = new Profile_fragment();
+
+    public ArrayList<Items_In_Site> items_in_sites = new ArrayList<>();
 
     BottomNavigationView bottomNavigationView;
 
@@ -25,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         flfragment = findViewById(R.id.flfragment);
+        //set the first fragment when the app starts
         setCurrentFragment(main_fragment);
 
+        //create the navigation menu and set onclick events for every button on the menu
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -43,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
     }
+
     //Add the fragment in the Frame layout
     public void setCurrentFragment(Fragment fragment){
         getSupportFragmentManager().beginTransaction()
--- src/main/java/com/example/ptyxiakh/Main_Fragment.java
+++ src/main/java/com/example/ptyxiakh/Main_Fragment.java
@@ -1,11 +1,59 @@
 package com.example.ptyxiakh;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main_Fragment extends Fragment {
-    public Main_Fragment(){
-        super(R.layout.main_fragment);
 
+
+
+    public Main_Fragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        //open about fragment click on about button
+        ImageButton aboutusbtn = view.findViewById(R.id.about_us_button);
+
+        aboutusbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                About_fragment about = new About_fragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("title","all Ok till now");
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flfragment,about,null)
+                        .addToBackStack(null);
+                about.setArguments(bundle);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        return view;
     }
 
 }
