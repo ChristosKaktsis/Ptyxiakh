b3155fe922e1fd10b654646ae4d57fbe6adb70e2
ChristosKaktsis <christoskaktsis@gmail.com>
2021-03-10 21:57:30 +0200

    First Step XML only

---
+++ src/main/java/com/example/ptyxiakh/About_fragment.java
@@ -0,0 +1,48 @@
+package com.example.ptyxiakh;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class About_fragment extends Fragment {
+
+    TextView titleText;
+
+    public About_fragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.about_fragment, container, false);
+
+        Bundle bundle = getArguments();
+        String title = bundle.getString("title");
+
+        titleText = view.findViewById(R.id.textTitle);
+        titleText.setText(title);
+
+
+       return view;
+    }
+
+
+}
